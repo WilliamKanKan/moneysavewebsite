75b79d01dffa93ca95eb9caca29e38129616781e
mengmeng0531 <dongchengren001@gmail.com>
2023-06-16 20:30:54 +0800

    test

--- src/main/java/com/money/controller/LoginController.java
+++ src/main/java/com/money/controller/LoginController.java
@@ -1,60 +1,45 @@
 package com.money.controller;
 
-import com.money.entity.UserBean;
-import com.money.service.UserService;
+import com.money.entity.UserLoginBean;
+import com.money.service.UserLoginService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
 import javax.annotation.Resource;
 
 @Slf4j
 @RestController
+@CrossOrigin
 public class LoginController {
 
     //将Service注入Web层
     @Resource
-    UserService userService;
+    UserLoginService userLoginService;
 
-    //实现登录
-    @RequestMapping("/login")
-    public String show(){
-        return "login";
-    }
-
-    @RequestMapping(value = "/loginIn",method = RequestMethod.POST)
-    public String login(String username,String password){
-        UserBean userBean = userService.LoginIn(username, password);
-        log.info("username:{}",username);
-        log.info("password:{}",password);
+   // 实现登录
+    @PostMapping(value = "/loginIn")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
+        UserLoginBean userBean = userLoginService.userLogin(username, password);
         if(userBean!=null){
             return "success";
         }else {
             return "error";
         }
     }
-    @RequestMapping("/signup")
-    public String disp(){
-        return "signup";
-    }
 
     //实现注册功能
     @RequestMapping(value = "/register",method = RequestMethod.POST)
-    public String signUp(@RequestBody UserBean user) {
+    public String signUp(@RequestBody UserLoginBean user) {
             String username = user.getUsername();
             String password = user.getPassword();
             String confirmPassword = user.getConfirmPassword();
             String email = user.getEmail();
-            System.out.println("username: " + username);
-            System.out.println("password: " + password);
-            System.out.println("email: " + email);
-            System.out.println("confirmPassword: " + confirmPassword);
             if(password.equals(confirmPassword)){
-                userService.Insert(username, password, email);
+                userLoginService.Insert(username, password, email);
                 System.out.println("注册成功了");
                 return "success";
-            }else if(!password.equals(confirmPassword)){
-                return "fail";
             }else {
-                return "error";
+                return "fail";
             }
     }
 }
